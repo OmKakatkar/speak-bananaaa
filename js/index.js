121d47b596d2c66542319b4e5a0bc0b60d5a183b
Om Kakatkar <omkakatkar@gmail.com>
2021-08-21 18:35:44 +0530

    error handling added

--- js/index.js
+++ js/index.js
@@ -6,22 +6,24 @@ let outputText = document.querySelector("#output-txt");
 
 let url = "https://api.funtranslations.com/translate/minion.json";
 
-
-function getTranslationUrl(text)
-{
-    return url + "?" + "text=" + text;
+function getTranslationUrl(text) {
+  return url + "?" + "text=" + text;
 }
 
-function clickEventHandler()
-{
-    console.log("clicked");
-    fetch(getTranslationUrl(inputText.value))
-        .then(response => response.json())
-        .then(json =>
-        {
-            let outputVal = json.contents.translated
-            outputText.innerText = outputVal
-        })
+function clickEventHandler() {
+  console.log("clicked");
+  fetch(getTranslationUrl(inputText.value))
+    .then((response) => response.json())
+    .then((json) => {
+      let outputVal = json.contents.translated;
+      outputText.innerText = outputVal;
+    })
+    .catch(errorHandler);
+}
+
+function errorHandler(error) {
+  console.log("Error occurred", error);
+  alert("Something went wrong, try again after a while");
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
